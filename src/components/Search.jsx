1c84ff6ce91128c7525d593d54070c9a42299b4d
Róbert Valcsák <robertvalcsak93@gmail.com>
2023-05-05 00:01:07 +0200

    css finished

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,62 +6,77 @@ const url = "https://api.weatherapi.com/v1/current.json?";
 function Search() {
   const [data, setData] = useState(null);
   const [city, setCity] = useState("");
+  const [img, setImg] = useState("cloud");
 
   const handleClick = () => {
     fetch(`${url}key=${apiKey}&q=${city}`)
       .then((res) => res.json())
       .then((data) => setData(data));
-    setCity("");
+      setImg(city)
+      setCity("");
   };
 
   return (
-    <div>
+    <div className="background" 
+    style={{ backgroundImage: `url(https://source.unsplash.com/1920x1080/?${img})`}}>
+    <div className="box">
       <div className="search">
         <input
           className="input"
           type="text"
-          placeholder="Search for cities"
+          placeholder="Search for city"
           onChange={(e) => setCity(e.target.value)}
           value={city}
         />
-        <button onClick={handleClick} />
+        <button onClick={handleClick}>
+          search
+          </button>
       </div>
       {data === null ? (
-        <p>Write a city name to see the weather.</p>
+        <p className="emptySearch">
+          Write a city name to see the weather.
+          </p>
       ) : (
         <div className="result">
           <h1>
-            Country:
-            <br />
-            {data.location.country}
+            Weather in {data.location.name}, {data.location.country}
           </h1>
+        <div className="temperature">  
           <h1>
-            Region:
-            <br />
-            {data.location.region}
-          </h1>
-          <h1>
-            City: <br />
-            {data.location.name}
+            Condition <br/>
+            {data.current.condition.text} <br/>
+            <img alt="" src={data.current.condition.icon}/>
           </h1>
           <h1>
             Temperature is <br />
-            {data.current.temp_c} C°
+            {data.current.temp_c} °C
           </h1>
           <h1>
-            wind speed is <br />
+            Temperature feels like <br/>
+            {data.current.feelslike_c} °C
+          </h1>
+          <h1>
+            Wind speed <br />
             {data.current.wind_kph} km/h
           </h1>
+          </div>
+          <div className="airCondition">
           <h1>
-            wind direction is: <br />
-            {data.current.wind_dir}
+            UV <br />
+            {data.current.uv}
           </h1>
-          <h2>
-            Cloud is <br />
-            {data.current.cloud}
-          </h2>
+          <h1>
+            Pressure <br/>
+            {data.current.pressure_mb} mb
+          </h1>
+          <h1>
+            Humidity <br />
+            {data.current.humidity} %
+          </h1>
+          </div>
         </div>
       )}
+    </div>
     </div>
   );
 }
